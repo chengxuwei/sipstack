bcbb1b620cbd24d19a2bbfa019476a619347d6d7
程绪伟 <cheng_xuwei@qq.com>
2025-01-21 12:04:21 +0800

    更新到sip

--- sipstack-example/src/main/java/io/sipstack/example/basic/UAS.java
+++ sipstack-example/src/main/java/io/sipstack/example/basic/UAS.java
@@ -3,6 +3,7 @@
  */
 package io.sipstack.example.basic;
 
+
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.ChannelHandler.Sharable;
 import io.netty.channel.ChannelHandlerContext;
@@ -18,6 +19,9 @@ import io.pkts.packet.sip.SipResponse;
 import io.sipstack.netty.codec.sip.SipMessageDatagramDecoder;
 import io.sipstack.netty.codec.sip.SipMessageEncoder;
 import io.sipstack.netty.codec.sip.SipMessageEvent;
+
+import lombok.extern.slf4j.Slf4j;
+
 
 import java.net.InetSocketAddress;
 
@@ -36,7 +40,9 @@ import java.net.InetSocketAddress;
  *
  */
 @Sharable
-public final class UAS extends SimpleChannelInboundHandler<SipMessageEvent> {
+@Slf4j
+
+public final class UAS extends SimpleChannelInboundHandler<SipMessageEvent>  {
 
     @Override
     protected void channelRead0(final ChannelHandlerContext ctx, final SipMessageEvent event) throws Exception {
@@ -52,27 +58,37 @@ public final class UAS extends SimpleChannelInboundHandler<SipMessageEvent> {
             final SipResponse response = msg.createResponse(200);
             event.getConnection().send(response);
         }
+        log.info("UAS消息:{}",event);
     }
 
-    public static void main(final String[] args) throws Exception {
+    private void start() throws InterruptedException {
         final UAS uas = new UAS();
         final EventLoopGroup udpGroup = new NioEventLoopGroup();
 
         final Bootstrap b = new Bootstrap();
         b.group(udpGroup)
-        .channel(NioDatagramChannel.class)
-        .handler(new ChannelInitializer<DatagramChannel>() {
-            @Override
-            protected void initChannel(final DatagramChannel ch) throws Exception {
-                final ChannelPipeline pipeline = ch.pipeline();
-                pipeline.addLast("decoder", new SipMessageDatagramDecoder());
-                pipeline.addLast("encoder", new SipMessageEncoder());
-                pipeline.addLast("handler", uas);
-            }
-        });
+                .channel(NioDatagramChannel.class)
+                .handler(new ChannelInitializer<DatagramChannel>() {
+                    @Override
+                    protected void initChannel(final DatagramChannel ch) throws Exception {
+                        final ChannelPipeline pipeline = ch.pipeline();
+                        pipeline.addLast("decoder", new SipMessageDatagramDecoder());
+                        pipeline.addLast("encoder", new SipMessageEncoder());
+                        pipeline.addLast("handler", uas);
+                    }
+                });
 
         final InetSocketAddress socketAddress = new InetSocketAddress("127.0.0.1", 5060);
         b.bind(socketAddress).sync().channel().closeFuture().await();
     }
+
+    public static void main(final String[] args) throws Exception {
+        log.info("启动服务器");
+        UAS uas=new UAS();
+        uas.start();
+
+    }
+
+
 
 }
